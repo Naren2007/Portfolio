49a2058b818b5a07140065b03f2afc8a2fa574e4
Naren2007 <narenjeeaspirant@gmail.com>
2024-05-25 19:56:37 +0530

    installation

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import MobileNav from './mobileNav/MobileNav'
+const Navbar = () => {
+  const [Openmenu,SetOpenmenu] = useState(false)
+  const togglemenu = ()=>{
+    SetOpenmenu(!Openmenu)
+  }
+  return (
+    <>
+    <MobileNav isOpen={Openmenu} togglemenu={togglemenu}/>
+      <nav className='nav-wrapper'>
+        <div className='nav-content'>
+            <img className="logo" src="" alt="" />
+            <ul>
+              <li>
+                 <a className="menu-item" href="">Home</a>
+              </li>
+              <li>
+                 <a className="menu-item" href="">Skills</a>
+              </li>
+              <li>
+                 <a className="menu-item" href="">Work Experience</a>
+              </li>
+              <li>
+                 <a className="menu-item" href="">Contact me</a>
+              </li>
+              <button className='hire-btn' onClick={()=>{}}>HIRE ME</button>
+            </ul>
+            <button onClick={togglemenu} className='menu-btn'>
+              <span class={'material-symbols-outlined'} style={{fontSize:"1.5rem"}}> {Openmenu ? "close" : "menu"}</span>
+            </button>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/Components/mobileNav/MobileNav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './MobileNav.css'
+const MobileNav = ({ isOpen, togglemenu}) => {
+  return (
+    <>
+      <div className={`mobile-menu ${isOpen ? "active":""}`}>
+        <div className="mobile-menu-container">
+            <img className='logo' src="" alt="" />
+            <ul>
+                <li>
+                   <a className="menu-item" href="menu-item">Home</a>
+                </li>
+                <li>
+                   <a className="menu-item" href="">Skills</a>
+                </li>
+                <li>
+                   <a className="menu-item" href="">Work Experience</a>
+                </li>
+                <li>
+                   <a className="menu-item" href="">Contact me</a>
+                </li>
+                <button className='hire-btn' onClick={()=>{}}>HIRE ME</button>
+            </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MobileNav
