db83c2ac7230c1ba8ca6cf08f785b92c312ef8b8
Naren2007 <166714842+Naren2007@users.noreply.github.com>
2024-05-27 14:32:33 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
                  <a className="menu-item" href="#home">Home</a>
               </li>
               <li>
-                 <a className="menu-item" href="#skillz">Skills</a>
+                 <a className="menu-item" href="#skills">Skills</a>
               </li>
               <li>
                  <a className="menu-item" href="#we">Work Experience</a>
