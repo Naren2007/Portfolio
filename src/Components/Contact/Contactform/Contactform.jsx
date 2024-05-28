da8c66b5bba5a8b3695357cae2a991adc7b908c4
Naren2007 <narenjeeaspirant@gmail.com>
2024-05-28 14:20:30 +0530

    Feature : added email facility

--- src/Components/Contact/Contactform/Contactform.jsx
+++ src/Components/Contact/Contactform/Contactform.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './Contactform.css'
 const Contactform = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_lg1i9bu', 'template_ji705k8', form.current, {
+        publicKey: 'WuJXUNuUO_oVnPO6v',
+      })
+      .then(
+        () => {
+          alert("EMAIL SENT")
+        },
+        (error) => {
+          alert('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <div className="contact-form-content">
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
             <div className="name-container">
-                <input type="text" name="firstname" placeholder='First Name' />
-                <input type="text" name="lastname" placeholder='Last Name' />
+                <input type="text" name="user_name" placeholder='Name' required/>
             </div>
-            <input type="email" name="email" placeholder='Email' />
-            <textarea name="message" rows={3} placeholder='Type Message'></textarea>
-            <button>SEND</button>
+            <input type="email" name="user_email" placeholder='Email' required/>
+            <textarea name="message" rows={3} placeholder='Type Message' required></textarea>
+            <button type='submit'>SEND</button>
         </form>
     </div>
   )
